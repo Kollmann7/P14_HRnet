26c5f88c92026bb5720945dd565ac17b07a4b641
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-11-02 18:03:58 +0100

    update employee form

--- src/components/employeeForm/employeeForm.js
+++ src/components/employeeForm/employeeForm.js
@@ -1,16 +1,20 @@
 import './employeeForm.css'
-import * as React from 'react';
+import React from 'react'
 import dayjs from 'dayjs';
-import { useForm} from 'react-hook-form'
+import { useForm, Controller} from 'react-hook-form'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import TextField from '@mui/material/TextField'
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
 import axios from 'axios'
+import { states, departments } from '../../utils/constants'
+import { FormControl ,Select, MenuItem } from '@mui/material';
+
 
 export default function EmployeeForm () {
-  const {register, handleSubmit} = useForm()
-  const [value, setValue] = React.useState(dayjs())
+  const {control, register, handleSubmit} = useForm()
+  const [startDate, setStartDate] = React.useState(dayjs())
+  const [birth, setBirth] = React.useState(dayjs())
 
   const onSubmit = (employee) => {
     axios.post('http://localhost:4000/employees',
@@ -35,23 +39,23 @@ export default function EmployeeForm () {
           <div className='date-wrapper'>
             <label>Start Date</label>
             <DesktopDatePicker
-            value={value}
+            value={startDate}
             minDate={dayjs('2000-01-01')}
             onChange={(newValue) => {
-              setValue(newValue)
+              setStartDate(newValue)
             }}
-            renderInput={(params) => <TextField {...params}  {...register('birth')}/>}
+            renderInput={(params) => <TextField {...params}  {...register('startDate')}/>}
             />
           </div>
           <div className='date-wrapper'>
             <label>Date of Birth</label>
             <DesktopDatePicker
-              value={value}
-              minDate={dayjs('2000-01-01')}
+              value={birth}
+              minDate={dayjs('1901-01-01')}
               onChange={(newValue) => {
-                setValue(newValue)
+                setBirth(newValue)
               }}
-              renderInput={(params) => <TextField {...params}  {...register('startDate')}/>}
+              renderInput={(params) => <TextField {...params}  {...register('birth')}/>}
             />
           </div>
         </LocalizationProvider>
@@ -65,24 +69,62 @@ export default function EmployeeForm () {
           <input  {...register('city')} />
 
           <label htmlFor="state">State</label>
-          <select {...register('state')}></select>
-
+          <FormControl  sx={{ m: 1, width: 300 }}>
+            <Controller
+              control={control}
+              name="State"
+              inputRef={register()}
+              defaultValue={states[0].abbreviation}
+              render={({ field: { onChange, value } }) => (
+                <Select
+                defaultValue={states[0].name}
+                value={value}
+                onChange={onChange}
+              >
+                {states.map((state) => (
+                <MenuItem
+                  key={state.name}
+                  value={state.abbreviation}
+                >
+                  {state.name}
+                </MenuItem>
+                ))}
+                </Select>
+              )}
+            />
+          </FormControl>
           <label htmlFor="zip-code">Zip Code</label>
           <input {...register('zipCode')}/>
         </fieldset>
-
         <label className='department' htmlFor="department">Department</label>
-          <select {...register('department')}>
-            <option>Sales</option>
-            <option>Marketing</option>
-            <option>Engineering</option>
-            <option>Human Resources</option>
-            <option>Legal</option>
-          </select>
-
-        <button className='submit-button' >Save</button>
+        <FormControl  sx={{ m: 1, width: 300 }}>
+          <Controller
+            control={control}
+            name="department"
+            inputRef={register()}
+            defaultValue={departments[0].value}
+            render={({ field: { onChange, value } }) => (
+              <Select
+              defaultValue={departments[0].label}
+              value={value}
+              onChange={onChange}
+            >
+              {departments.map((department) => (
+              <MenuItem
+                key={department.label}
+                value={department.value}
+              >
+                {department.label}
+              </MenuItem>
+              ))}
+              </Select>
+            )}
+          />
+        </FormControl>
+        <div className='button-container'>
+          <button className='submit-button' >Save</button>
+        </div>
       </form>
-
     </>
   )
 }
